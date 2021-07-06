cdf5702fd0ba2fc6e0c64892f41f2668cfd28b64
pishchulenok <64210553+sirejja@users.noreply.github.com>
2021-07-07 00:31:57 +0300

    queries added

--- coursera_assignment_tmp-master/db/query.py
+++ coursera_assignment_tmp-master/db/query.py
@@ -7,60 +7,78 @@ from db.models import User, Blog, Topic
 
 
 def create():
-    pass
+    u1 = User.objects.create(first_name='u1', last_name='u1')
+    u2 = User.objects.create(first_name='u2', last_name='u2')
+    u3 = User.objects.create(first_name='u3', last_name='u3')
+
+    blog1 = Blog.objects.create(title='blog1', author=u1)
+    blog2 = Blog.objects.create(title='blog2', author=u1)
+
+    blog1.subscribers.add(u1, u2)
+    blog2.subscribers.add(u2)
+
+    topic1 = Topic.objects.create(
+        title='topic1', blog=blog1, author=u1
+    )
+    topic2 = Topic.objects.create(
+        title='topic2_content', blog=blog1, author=u3, created='2017-01-01'
+    )
+    topic1.likes.add(u1, u2, u3)
 
 
 def edit_all():
-    pass
+    User.objects.all().update(first_name='uu1')
 
 
 def edit_u1_u2():
-    pass
+    User.objects.filter(
+        Q(first_name='u1') | Q(first_name='u2')).update(first_name='uu1')
 
 
 def delete_u1():
-    pass
+    User.objects.filter(first_name='u1').delete()
 
 
 def unsubscribe_u2_from_blogs():
-    pass
+    u = User.objects.get(first_name='u2')
+    u.subscriptions.clear()
 
 
 def get_topic_created_grated():
-    pass
+    return Topic.objects.filter(created__gte='2018-01-01')
 
 
 def get_topic_title_ended():
-    pass
+    return Topic.objects.filter(title__endswith="content")
 
 
 def get_user_with_limit():
-    pass
+    return User.objects.all().order_by('-id')[:2]
 
 
 def get_topic_count():
-    pass
+    return Blog.objects.all().annotate(topic_count=Count('topic')).order_by('topic_count')
 
 
 def get_avg_topic_count():
-    pass
+    return Blog.objects.all().annotate(count=Count('topic')).aggregate(avg=Avg('count'))
 
 
 def get_blog_that_have_more_than_one_topic():
-    pass
+    return Blog.objects.all().annotate(count=Count('topic')).filter(count__gt=1)
 
 
 def get_topic_by_u1():
-    pass
+    return Topic.objects.filter(author__first_name='u1')
 
 
 def get_user_that_dont_have_blog():
-    pass
+    return User.objects.all().filter(blog__isnull=True).order_by('pk')
 
 
 def get_topic_that_like_all_users():
-    pass
+    return Topic.objects.filter(likes=User.objects.all())
 
 
 def get_topic_that_dont_have_like():
-    pass
+    return Topic.objects.filter(likes__isnull=True)
